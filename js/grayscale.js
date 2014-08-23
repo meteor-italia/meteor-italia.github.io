9b8fea96751f3aa0e2afe2257186b59f613f2732
grigio <grigio.org@gmail.com>
2014-08-23 12:53:13 +0200

    fix anchor name in location

--- js/grayscale.js
+++ js/grayscale.js
@@ -14,6 +14,7 @@ $(function() {
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
+        location.hash = $anchor.attr('href');
         event.preventDefault();
     });
 });
